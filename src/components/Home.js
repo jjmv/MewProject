8f86785b3b3ac4938fc3887f9f54adc0ac9a12aa
Javier <bitbucket@bmtask.com>
2018-06-12 14:37:41 -0500

    Testing routes

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    static propTypes = {
+        
+    }
+    
+    static defaultProps = {
+        
+    }
+    
+    render() {
+        return(
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default Home
