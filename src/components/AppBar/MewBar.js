9da86ca3a7b3b7a86f32f4520dac26f8b62ef8a6
Javier <bitbucket@bmtask.com>
2018-06-15 16:44:14 -0500

    To work at home

--- src/components/AppBar/MewBar.js
+++ src/components/AppBar/MewBar.js
@@ -8,9 +8,15 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-// const styles = {
-//
-// }
+const style = {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  borderRadius: 3,
+  border: 0,
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+};
 
 class MewBar extends Component {
     // static propTypes = {
@@ -22,7 +28,7 @@ class MewBar extends Component {
     render(){
         return(
             <div>
-                <AppBar position="static">
+                <AppBar style={style} position="static">
                     <Toolbar>
                         <IconButton color="inherit" aria-label="Menu">
                             <MenuIcon />
